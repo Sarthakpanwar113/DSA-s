64d4ff8f4efa3113e594d93bc3dea7b175835400
sarthakpanwar <exclusivethought@gmail.com>
2023-09-17 12:40:07 +0530

    sort0 and i's using 2 pointer

--- week_2/array/6.Sort0'sand1's.cpp
+++ week_2/array/6.Sort0'sand1's.cpp
@@ -1,47 +1,80 @@
-// we have sort all zero's and one's from the array  zero's to the left and one's to the right
-// 1|0|0|0|1|1|0|1|0|0|1
+// // we have sort all zero's and one's from the array  zero's to the left and one's to the right
+// // 1|0|0|0|1|1|0|1|0|0|1
 
-#include <iostream>
+// #include <iostream>
+// using namespace std;
+
+// void sortallzeroandone(int arr[], int n)
+// {
+//     int zerocount = 0;
+//     int onecount = 0;
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (arr[i] == 0)
+//         {
+//             zerocount = zerocount + 1;
+//         }
+//         else
+//         {
+//             onecount = onecount + 1;
+//         }
+//     }
+//     cout << zerocount << endl;
+//     cout << onecount << endl;
+
+//     int i;
+//     for (i = 0; i < zerocount; i++)
+//     {
+//         arr[i] = 0;
+//     }
+
+//     for (int j = i; j < n; j++)
+//     {
+//         arr[j] = 1;
+//     }
+// }
+
+// int main()
+// {
+//     int arr[] = {0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1};
+//     int n = 12;
+//     sortallzeroandone(arr, n);
+//     // cout<<"sort array will be";
+//     // printing
+//     for(int i=0; i<n; i++){
+//         cout<<arr[i]<<",";
+//     }
+// }
+
+
+
+// two pointer approch 
+#include<iostream>
 using namespace std;
 
-void sortallzeroandone(int arr[], int n)
-{
-    int zerocount = 0;
-    int onecount = 0;
-    for (int i = 0; i < n; i++)
-    {
-        if (arr[i] == 0)
-        {
-            zerocount = zerocount + 1;
+void sortallzeroandone(int arr[], int n){
+    int start = 0;
+    int end = n-1;
+    while(start<end){
+        if(arr[start]==0){
+            start++;
         }
-        else
-        {
-            onecount = onecount + 1;
+        else if(arr[end]==1){
+            end--;
         }
-    }
-    cout << zerocount << endl;
-    cout << onecount << endl;
+        else{
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+}
+    }    
 
-    int i;
-    for (i = 0; i < zerocount; i++)
-    {
-        arr[i] = 0;
-    }
-
-    for (int j = i; j < n; j++)
-    {
-        arr[j] = 1;
-    }
 }
 
-int main()
-{
+int main(){
+    
     int arr[] = {0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1};
     int n = 12;
     sortallzeroandone(arr, n);
-    // cout<<"sort array will be";
-    // printing
-    for(int i=0; i<n; i++){
-        cout<<arr[i]<<",";
-    }
+    cout<<sortallzeroandone;
 }
